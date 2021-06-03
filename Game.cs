73db8000d810e1c8b7dfc0d5b02c36b93f9f3d29
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-03 20:11:19 +0530

    [Harsh]add:- new class Game is added

---
+++ Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("....|WELCOME TO TIC TAC TOE|....");
+        }
+
+        public void createBoard()
+        {
+
+            char[] board = new char[10];        // initializing the number of character spaces in game
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';                // making each space empty 
+
+            }
+        }
+        public void Display()
+        {
+
+        }
+    }
+}
