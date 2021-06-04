4a67fd05621122ebc53b11449e7a54460acb6da5
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-04 18:50:45 +0530

    [Harsh]Refactor:-New method for checking empty spaces

--- Game.cs
+++ Game.cs
@@ -6,75 +6,127 @@ namespace TicTacToe
 {
     class Game
     {
+        const int HEADS = 1;
+        const int TAILS = 0;
+        public enum Player { USER, COMPUTER };
+        //welcome message
         public void Welcome()
         {
-            Console.WriteLine("....|WELCOME TO TIC TAC TOE|....");
+            Console.WriteLine("~~~~~~TIC TAC TOE~~~~~~");
         }
 
-        public char[] CreateBoard()
+        public char[] GameBoard()
         {
-            char[] board = new char[10];        // initializing the number of character spaces in game
+            //game board intilized with empty spaces and
+            //zeroth index ignored
+            char[] board = new char[10];
+
             for (int i = 1; i < board.Length; i++)
             {
-                board[i] = ' ';                // making each space empty 
-
+                board[i] = ' ';
             }
             return board;
         }
-       
-        
 
-        public static int Toss()
+        //tossing for players
+        private static int Toss()
         {
             Random random = new Random();
-            int ran = random.Next(2);
-            if(ran == 0)
+            int rand = random.Next(2);
+            if (rand == HEADS)
             {
-                Console.WriteLine("Player 1 plays first ");
-                Console.WriteLine("Player 1 will use X for marking ");
+                Console.WriteLine("Computer plays first");
+                Console.WriteLine("computer inputs 'X' ");
+                return HEADS;
             }
-            if(ran == 1)
+            if (rand == TAILS)
             {
-                Console.WriteLine("Player 2 plays first");
-                Console.WriteLine("Player 2 inputs with O");
+                Console.WriteLine("Player plays first");
+                Console.WriteLine("player inputs 'O' ");
+                return TAILS;
             }
-            return ran;
+            return 0;
         }
-        
-        public void ShowBoard(char [] board)
-        {
-            Console.WriteLine(" " + board[1] + "|" + board[2] + "|" + board[3]);
-            Console.WriteLine("_________");
-            Console.WriteLine(" " + board[4] + "|" + board[5] + "|" + board[6]);
-            Console.WriteLine("_________");
-            Console.WriteLine(" " + board[7] + "|" + board[8] + "|" + board[9]);
 
-        }
-        public void Input()
+        //displaying game board
+        public void ShowBoard(char[] board)
         {
-            Toss();
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {board[1]} | {board[2]} | {board[3]}");
+            Console.WriteLine("___|___|___");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {board[4]} | {board[5]} | {board[6]}");
+            Console.WriteLine("___|___|___");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {board[7]} | {board[8]} | {board[9]}");
+            Console.WriteLine("   |   |   ");
         }
-        private static int getUserMove(char[] board)
+        //user making his move
+        public char[] UserMove(char[] board, Player player)
         {
-            int[] validCells = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("select the location on Board :  ");
+            int location = Convert.ToInt32(Console.ReadLine());
+            //validating user location
+            while (location > 9 || location < 1)
+            {
+                Console.WriteLine("invalid location, select the location on Board :  ");
+                location = Convert.ToInt32(Console.ReadLine());
+            }
+            //loop to check for empty space
+            bool one = false;
             while (true)
             {
-                Console.WriteLine("Enter is your move (1,9): ");
-                int index = Convert.ToInt32(Console.ReadLine());
-                if (Array.Find<int>(validCells, elements => elements == index) != 0 && isSpaceFree(board, index))
-                    return index;
-
+                //checking for free space
+                location = CheckBoard(location, board);
+                if (location == 0)
+                {
+                    Console.WriteLine("location already selected");
+                    Console.WriteLine("select another location on Board :  ");
+                    location = Convert.ToInt32(Console.ReadLine());
+                    one = true;
+                }
+                if (player == Player.USER)
+                {
+                    board[location] = 'X';
+                    break;
+                }
+                if (player == Player.COMPUTER)
+                {
+                    board[location] = 'O';
+                    break;
+                }
             }
+            return board;
+        }
 
-        }
-        private static bool isSpaceFree(char[] board, int index)
+        //checking if board is empty
+        public int CheckBoard(int location, char[] board)
         {
-            return board[index] == ' ';
+            if (board[location] != ' ')
+            {
+                return 0;
+            }
+            else
+            {
+                return location;
+            }
         }
-        private static void makeMove(char[] board, int index, char letter)   //method to check for blank space 
+
+        public Player GameInput()
         {
-            Boolean SpaceFree = isSpaceFree(board, index);  //Getting values from parameters
-            if (SpaceFree) board[index] = letter;           //condition check
+            //randomly selecting who plays first
+            // and assign character x and o
+
+            int playerSel = Toss();
+            Player player;
+            if (playerSel == HEADS)
+            {
+                return player = Player.COMPUTER;
+            }
+            else
+                return player = Player.USER;
+
+
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -6,14 +6,21 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Welcome to TicTacToe game");
             Game game = new Game();
             game.Welcome();
-            Console.WriteLine();
-            game.CreateBoard();
-            char[] board = game.CreateBoard();
-            game.Input();
+            Console.WriteLine("");
+            char[] board = game.GameBoard();
+            Game.Player player = game.GameInput();
             game.ShowBoard(board);
-            
+            //loop till all empty spaces are filled
+            for (int i = 1; i < board.Length; i++)
+            {
+                board = game.UserMove(board, player);
+                game.ShowBoard(board);
+            }
+
+
         }
     }
 }
